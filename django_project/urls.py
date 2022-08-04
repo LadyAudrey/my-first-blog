ac070cc8f6765b4d950c0a9a9432e8a50cfd980b
LadyAudrey <theaudreygreene@gmail.com>
2022-08-04 16:31:24 -0700

    fixing Settings fiasco

--- django_project/urls.py
+++ django_project/urls.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path('admin/', admin.site.urls),
-               path('', include('pages.urls')),
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include("pages.urls")),
 ]
-
-#urlpatterns = [
-#    path('admin/', admin.site.urls),
-#]
