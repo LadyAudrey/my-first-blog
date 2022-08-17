d51ce8cdd08c4afea1215c505de7f0471b67617c
LadyAudrey <theaudreygreene@gmail.com>
2022-08-17 13:30:25 -0700

    figuring it out

--- django_project/settings.py
+++ django_project/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pages.apps.PagesConfig',
-    'posts.apps.PostsConfig',
+    #'pages.apps.PagesConfig',
+    #'posts.apps.PostsConfig',
     'blogs.apps.BlogsConfig',
 ]
 
--- django_project/urls.py
+++ django_project/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("pages.urls")),
     path('', include("blogs.urls")),
+    #path('', include("pages.urls")), #do I need to comment this out for blogs to be visible?
 ]
